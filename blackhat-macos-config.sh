2a0a525add2798710894e43cf0df51f9efbbca4c
Samy Kamkar <git@samy.pl>
2018-08-04 17:08:20 -0700

    Fix bug where firewall won't get enabled w/o sudo

--- blackhat-macos-config.sh
+++ blackhat-macos-config.sh
@@ -24,15 +24,13 @@ if [ $? -ne 0 ]; then
     printf "\n"
 fi
 
-exit
-
 #Delete Saved SSIDs For Security
 #Be Sure To Set Home And Work SSID for ease of use.
 printf "Deleting saved Wi-Fi networks.\n"
 IFS=$'\n'
 for ssid in $(networksetup -listpreferredwirelessnetworks en0 | grep -v "Preferred networks on en0:" | grep -v $homessid | grep -v $workssid | sed "s/[\	]//g")
 do
-    networksetup -removepreferredwirelessnetwork en0 "$ssid"  > /dev/null 2>&1
+    networksetup -removepreferredwirelessnetwork en0 "$ssid" > /dev/null 2>&1
 done
 
 #Enabling FileVault:
@@ -41,7 +39,7 @@ fdesetup enable > /dev/null 2>&1
 
 #Enable Firewall:
 printf "Enabling firewall.\n"
-defaults write /Library/Preferences/com.apple.alf globalstate 1  > /dev/null 2>&1
+sudo defaults write /Library/Preferences/com.apple.alf globalstate 1 > /dev/null 2>&1
 
 #Install Updates.
 printf "Installing needed updates.\n"
